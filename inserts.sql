6a8383e15851c12eff6a5c0c3fd1745b0fb98b6d
Chris Frashure <csfrashure@gmail.com>
2018-11-03 17:51:16 -0400

    added database inserts file

---
+++ inserts.sql
@@ -0,0 +1,12 @@
+insert into parties (partyName, partyColor)
+values 
+('Democratic', 'blue'),
+('Libertarian', 'yellow'),
+('Republican', 'red'),
+('Green', 'green');
+
+insert into candidates (fName, lName, incumbent, party_id)
+values
+('Donald', 'Trump', true, 6),
+('Andrew', 'Yang', false, 4),
+('Arvin', 'Vohra', false, 6);
