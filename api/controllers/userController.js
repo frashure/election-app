92cfbf51a3f5e83d13b1de816394a8329ca9a088
Chris Frashure <csfrashure@gmail.com>
2018-11-06 21:41:06 -0500

    fixed validation, other minor updates

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require('../models/dbconnection');
+
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -12,14 +13,16 @@ var userController = {
         let fName = req.body.firstName;
         let lName = req.body.lastName;
         let passwordMatch = req.body.passwordMatch;
-        let dateRegistered = new Date();
+        let dateRegistered = new Date();    
         
 
         req.check('email', 'Email field cannot be empty.').not().isEmpty();
         req.check('email', 'Invalid email').isEmail();
+        req.check('firstName', 'First name is required.').not().isEmpty();
+        req.check('lastName', 'Last name is required.').not().isEmpty();
         req.check('password', 'Password field is required').not().isEmpty();
         req.check('password', 'Password must be between 8-100 characters.').isLength(8, 100);
-        req.check('passwordMatch', 'Passwords do not match.').equals(req.body.password);
+        req.check('passwordMatch', 'Passwords do not match.').equals(req.body.passwordMatch);
 
 
         let errors = req.validationErrors();
@@ -27,10 +30,14 @@ var userController = {
         if (errors) {
             console.log(errors);
             res.send(errors);
+            console.log(req.body.password);
+            console.log(password);
+            console.log(req.body.email);
+            console.log(email);
 
         }
         else {
-            bcrypt.hash(password, saltRounds, function(err, hash) {
+            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
                 db.query('INSERT INTO voters (email, voter_password, date_registered, fName, lName) VALUES (?, ?, ?, ?, ?)', [email, hash, dateRegistered, fName, lName], function(err, results) {
                     if (err) {
                        console.log(err);
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,3 @@
-const { check, validationResult } = require('express-validator/check');
-
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 var controller = require('../controllers/userController');
@@ -7,7 +5,7 @@ var controller = require('../controllers/userController');
 ROUTER.get('/register', (req, res) => {res.redirect('../register.html')});
 ROUTER.post('/register', controller.register);
 
-ROUTER.get('/login', (req, res) => [res.redirect('../login.html')]);
+ROUTER.get('/login', (req, res) => {res.redirect('../login.html')});
 ROUTER.post('/login', controller.login)
 
 module.exports = ROUTER;
