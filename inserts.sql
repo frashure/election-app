e7c1a834815cadbff84f6160666719682c219d1a
Chris Frashure <csfrashure@gmail.com>
2018-12-07 17:38:32 -0500

    update sql inserts for candidates

--- inserts.sql
+++ inserts.sql
@@ -5,8 +5,14 @@ values
 ('Republican', 'red'),
 ('Green', 'green');
 
-insert into candidates (fName, lName, incumbent, party_id)
-values
-('Donald', 'Trump', true, 6),
-('Andrew', 'Yang', false, 4),
-('Arvin', 'Vohra', false, 6);
+insert into candidates (firstName, lastName, isActive, party_id)
+values 
+('John', 'Delaney', 1, 'DEM'),
+('Richard', 'Oeda', 1, 'DEM'),
+('Micheal', 'Arth', 1, 'DEM'),
+('Ken', 'Nwadike', 1, 'DEM'),
+('Bobby', 'Well', 1, 'DEM'),
+('Andrew', 'Yang', 1, 'DEM'),
+('Andrew', 'Kokesh', 1, 'LIB'),
+('Vermin', 'Supreme', 1, 'LIB'),
+('Jessie', 'Ventura', 1, 'GRN');
