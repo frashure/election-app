553c83dc412619c8c3e95cb3e5c66fdf62de4899
Chris Frashure <csfrashure@gmail.com>
2018-12-07 18:17:06 -0500

    sql inserts for candidates entity

---
+++ db_resources/candidate_inserts.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: select * from candidates
+LIMIT 0, 1000
+
+-- Date: 2018-12-07 18:03
+*/
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (1,'Donald','Trump','J',1,NULL,'REP');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (2,'Andrew','Yang',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (3,'Arvin','Vohra',NULL,1,NULL,'LIB');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (4,'John','Delaney',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (5,'Richard','Oeda',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (6,'Micheal','Arth',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (7,'Ken','Nwadike',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (8,'Bobby','Well',NULL,1,NULL,'DEM');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (10,'Andrew','Kokesh',NULL,1,NULL,'LIB');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (11,'Vermin','Supreme',NULL,1,NULL,'LIB');
+INSERT INTO `candidates` (`candidate_id`,`firstName`,`lastName`,`middleName`,`isActive`,`website`,`party_id`) VALUES (12,'Jessie','Ventura',NULL,1,NULL,'GRN');
