bbdf5ca7f6ad88dec2dc9b853af85cfadc159065
Chris Frashure <csfrashure@gmail.com>
2018-12-07 17:49:44 -0500

    moved all db related files from root folder

---
+++ db_resources/legislatures_inserts.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from legislatures
+LIMIT 0, 1000
+
+-- Date: 2018-12-07 17:47
+*/
+INSERT INTO `legislatures` (`legislature_id`,`fed_state`,`state`,`name`,`upper_lower`,`tenure`,`num_districts`) VALUES (1,'f','','President',NULL,4,NULL);
+INSERT INTO `legislatures` (`legislature_id`,`fed_state`,`state`,`name`,`upper_lower`,`tenure`,`num_districts`) VALUES (2,'f','','US House','lower',2,'435');
+INSERT INTO `legislatures` (`legislature_id`,`fed_state`,`state`,`name`,`upper_lower`,`tenure`,`num_districts`) VALUES (3,'f','','US Senate','upper',6,'100');
+INSERT INTO `legislatures` (`legislature_id`,`fed_state`,`state`,`name`,`upper_lower`,`tenure`,`num_districts`) VALUES (4,'s','VA','Senate','upper',4,'40');
+INSERT INTO `legislatures` (`legislature_id`,`fed_state`,`state`,`name`,`upper_lower`,`tenure`,`num_districts`) VALUES (5,'s','VA','House of Representatives','lower',2,'60');
---
+++ db_resources/offices_inserts.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: select * from offices
+LIMIT 0, 1000
+
+-- Date: 2018-12-07 17:48
+*/
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (1,NULL,1,NULL);
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (2,1,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (3,2,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (4,3,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (5,4,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (6,5,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (7,6,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (8,7,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (9,8,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (10,9,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (11,10,2,'VA');
+INSERT INTO `offices` (`office_id`,`district`,`legislature_id`,`state`) VALUES (12,11,2,'VA');
