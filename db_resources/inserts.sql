d1cca97911fe038ab238581814f23107876e78fc
Chris Frashure <csfrashure@gmail.com>
2018-12-07 20:24:22 -0500

    moved db resources

---
+++ db_resources/inserts.sql
@@ -0,0 +1,18 @@
+insert into parties (partyName, partyColor)
+values 
+('Democratic', 'blue'),
+('Libertarian', 'yellow'),
+('Republican', 'red'),
+('Green', 'green');
+
+insert into candidates (firstName, lastName, isActive, party_id)
+values 
+('John', 'Delaney', 1, 'DEM'),
+('Richard', 'Oeda', 1, 'DEM'),
+('Micheal', 'Arth', 1, 'DEM'),
+('Ken', 'Nwadike', 1, 'DEM'),
+('Bobby', 'Well', 1, 'DEM'),
+('Andrew', 'Yang', 1, 'DEM'),
+('Andrew', 'Kokesh', 1, 'LIB'),
+('Vermin', 'Supreme', 1, 'LIB'),
+('Jessie', 'Ventura', 1, 'GRN');
