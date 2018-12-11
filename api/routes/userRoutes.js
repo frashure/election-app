024dd0701f32569a4b8c0abe4b5734b8685059c3
Chris Frashure <csfrashure@gmail.com>
2018-12-10 21:45:59 -0500

    added some auth, new route for getting user ballot

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,7 +2,10 @@ const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
 var controller = require('../controllers/userController');
 var passport = require('passport');
+var user = require('./../controllers/userController');
 
+
+ROUTER.get('/', controller.isLoggedIn, controller.getUserInfo);
 
 ROUTER.get('/register', (req, res) => {res.redirect('../register.html')});
 ROUTER.post('/register', controller.register);
@@ -10,13 +13,13 @@ ROUTER.post('/register', controller.register);
 ROUTER.get('/login', (req, res) => {res.redirect('../login.html')});
 ROUTER.post('/login', passport.authenticate('local', {successRedirect:'/dashboard.html', failureRedirect: '../login.html'}));
 
-ROUTER.get('/', controller.isLoggedIn, controller.getUserInfo);
-
 ROUTER.get('/logout', (req, res) => {
     req.logout();
     req.session.destroy();
     res.redirect('../');
 })
+
+ROUTER.get('/ballot', user.isLoggedIn, controller.getBallot);
 
 
 
