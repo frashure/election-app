3a4f7559dad326e32b68b64ec4111fc4aca35cc5
Chris Frashure <csfrashure@gmail.com>
2018-12-21 18:45:45 -0500

    changed column name in query

--- api/controllers/candidateController.js
+++ api/controllers/candidateController.js
@@ -5,7 +5,7 @@ var candidateController = {
 
     // Get all candidates
     getAllCandidates: (req, res) => {
-        var results = db.query(`SELECT * FROM candidates c 
+        var results = db.query(`SELECT c.*, p.name AS party_name, p.* FROM candidates c 
         LEFT JOIN election_candidates ec
         ON c.candidate_id = ec.candidate_id
         LEFT JOIN parties p ON c.party_id = p.party_id
@@ -33,10 +33,8 @@ var candidateController = {
     }, // End getCandidatesById
 
     getCandidatesByParty: (req, res) => {
-        var results = db.query(`SELECT * FROM
+        var results = db.query(`SELECT c.*, p.name AS party_name, p.party_id FROM
             candidates c
-            LEFT JOIN election_candidates ec
-            ON ec.candidate_id = c.candidate_id
             LEFT JOIN parties p
             ON c.party_id = p.party_id
             WHERE p.name = ?
@@ -50,7 +48,27 @@ var candidateController = {
             }
         } // end query callback 
         ) // end db.query
-    } // end getCandidatesByParty
+    }, // end getCandidatesByParty
+
+    getCandidatesByElection: (req, res) => {
+        var results = db.query(`SELECT c.*, p.name AS party_name, p.*
+        FROM candidates c
+            LEFT JOIN parties p
+                ON c.party_id = p.party_id
+        WHERE c.candidate_id IN (
+            SELECT candidate_id
+            FROM election_candidates
+            WHERE election_id = ?
+        )`, [req.params.election_id], (err, results) => {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            else {
+                res.json(results);
+            }
+        });
+    } // end getCandidatesByElection
 
     }; // End class
 
