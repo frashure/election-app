81322ffcffc9f35288ec05512afc36a9acfd8c4b
Chris Frashure <csfrashure@gmail.com>
2018-12-21 21:39:41 -0500

    added route for getting candidates by election and party

--- api/routes/candidatesRoutes.js
+++ api/routes/candidatesRoutes.js
@@ -14,7 +14,10 @@ ROUTER.get('/id/:id', candidatesController.getCandidatesById);
 ROUTER.get('/party/:party', candidatesController.getCandidatesByParty);
 
 // GET candidates by election only
-ROUTER.get('/election/:election_id', candidatesController.getCandidatesByElection);
+ROUTER.get('/election/:legislature_id', candidatesController.getCandidatesByElection);
+
+// GET candidates by election and party
+ROUTER.get('/election/:legislature_id/party/:party_name', candidatesController.getCandidatesByElectionParty);
 
 // GET all candidates by state
 
